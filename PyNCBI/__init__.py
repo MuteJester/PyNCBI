0222242057eff8cced40ba677ed34bc8f0a515a6
Thomas Konstantinovsky <56339082+MuteJester@users.noreply.github.com>
2022-05-20 12:04:43 +0300

    Added GSM Class Functionality

--- PyNCBI/__init__.py
+++ PyNCBI/__init__.py
@@ -5,4 +5,4 @@ from PyNCBI.GEOReader import GEOReader
 from PyNCBI.Utilities import *
 from PyNCBI.FileUtilities import *
 from PyNCBI.Constants import *
-from GSM import GSM
+from PyNCBI.GSM import GSM
